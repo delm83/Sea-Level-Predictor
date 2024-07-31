5cc972ab9f10afd67999c482bfc49e9d4f06a629
delm83 <delm83@googlemail.com>
2024-07-31 14:38:45 +0100

    completed second line of best fit and passed all tests

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -5,21 +5,33 @@ from scipy.stats import linregress
 def draw_plot():
     # Read data from file
     df = pd.read_csv('epa-sea-level.csv')
+    
+    #without this the legend produces duplicates
+    fig, ax = plt.subplots()
 
     # Create scatter plot
-    plt.scatter(x='Year', y='CSIRO Adjusted Sea Level', data=df)
+    plt.scatter(x='Year', y='CSIRO Adjusted Sea Level', data=df, label='recorded sea level')
  
     # Create first line of best fit
     res = linregress(x=df['Year'], y=df['CSIRO Adjusted Sea Level'])
-    plt.plot(df.Year, res.intercept + res.slope*df.Year, 'g')
+    # Create new year series for x axis to extend years to 2050
+    extended_years = pd.Series([year for year in range(1880, 2051)])
+    plt.plot(extended_years, res.intercept + res.slope*extended_years, 'g', label='prediction line of best fit 1880 - 2050')
     
     # Create second line of best fit
+    # Use only data from 2000
+    df_recent = df[df['Year']>1999]
+    res = linregress(x=df_recent['Year'], y=df_recent['CSIRO Adjusted Sea Level'])
+    # Use only a series of years from 2000 - 2050
+    recent_years = extended_years[extended_years>1999]
+    plt.plot(recent_years, res.intercept + res.slope*recent_years, 'r', label='prediction line of best fit 2000 - 2050')
 
     # Add labels and title
     plt.title('Rise in Sea Level')
     plt.xlabel('Year')
     plt.ylabel('Sea Level (inches)')
     plt.tight_layout()
+    plt.legend()
     
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
